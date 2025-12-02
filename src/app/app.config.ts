8b68ab9f47ddd0db88b6601f68b8a6cd74d10b2c
JuanGarcia25-dev <2019171031@uteq.edu.mx>
2025-12-02 12:12:29 -0600

    "Agregacion de PWA"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,9 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           })
   ]
 };
